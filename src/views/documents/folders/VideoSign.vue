09302a718f1be6056841da0534ac54d80efe24f5
Demola <adewuyi269@gmail.com>
2022-12-12 13:04:39 +0100

    Update on video sign component

--- src/views/documents/folders/VideoSign.vue
+++ src/views/documents/folders/VideoSign.vue
@@ -125,23 +125,24 @@
                           <Icon icon="oi:ellipses" :rotate="1" :verticalFlip="true" />
                         </a>
                         <div class="dropdown-menu dropdown-menu-end">
-                          <template v-if="result.status != 'completed'">
+                          <router-link :to="{ name: 'document.preview' }" class="dropdown-item" @click="
+                            getDocument({
+                              id: result.document.id,
+                            })
+                          ">
+                            <Icon icon="fontisto:preview" />
+                            Preview
+                          </router-link>
+                          <template v-if="result.status != 'Completed'">
                             <div @click="openSessionModal(result.id)" class="dropdown-item">
                               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="feather feather-edit font-small-4 me-50">
                                 <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                                 <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                              </svg>Reschedule
+                              </svg>
+                              Reschedule
                             </div>
-                            <router-link :to="{ name: 'document.preview' }" class="dropdown-item" @click="
-                              getDocument({
-                                id: result.document.id,
-                              })
-                            ">
-                              <Icon icon="fontisto:preview" />
-                              Preview
-                            </router-link>
                           </template>
                           <template v-else>
                             <div class="dropdown-item">
